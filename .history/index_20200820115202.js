fcf85dd92c8cf342b721f33b22e0a43d43c7e186
Charlie Pelton <charliejp0311@gmail.com>
2020-08-20 11:56:18 -0500

    Done.

---
+++ .history/index_20200820115202.js
@@ -0,0 +1,26 @@
+// Your code here
+class Cat {
+    //...
+
+    speak = () => {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    //...
+    speak = () => {
+        return `${this.name} says woof!`
+    }
+}
+
+class Bird {
+    //...
+    speak = () => {
+        if (this.male === true) {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`            
+        }
+    }
+}
--- .history/index_20200820115445.js
+++ .history/index_20200820115445.js
@@ -1,10 +1,7 @@
 // Your code here
 class Cat {
     //...
-    constructor (name,gender){
-        this.name = name;
-        this.gender = gender;
-    }
+    constructor (name,gender)
     speak(){
         return `${this.name} says meow!`
     }
@@ -12,10 +9,6 @@ class Cat {
 
 class Dog {
     //...
-    constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
-    }
     speak(){
         return `${this.name} says woof!`
     }
@@ -23,10 +16,6 @@ class Dog {
 
 class Bird {
     //...
-    constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
-    }
     speak(){
         if (this.gender === 'male') {
             return `It's me! ${this.name}, the parrot!`
